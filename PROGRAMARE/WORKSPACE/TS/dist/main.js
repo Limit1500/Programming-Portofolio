8c6d057e4c5d16889507f4dbb02ee1aecff8e7ca
Limit1500 <146538463+Limit1500@users.noreply.github.com>
2024-07-29 15:31:22 +0300

    D

---
+++ PROGRAMARE/WORKSPACE/TS/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 10;
+if (a == 10) {
+    a = a * 2;
+}
+console.log(a);
+//# sourceMappingURL=main.js.map
---
+++ PROGRAMARE/WORKSPACE/TS/src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 10;
+if (a == 10) {
+    a = a * 2;
+}
+console.log(a);
