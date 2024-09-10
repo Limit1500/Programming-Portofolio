482bcc27514be917e3b8c7cc4904d73d79e36f05
Limit1500 <david.tamas999@gmail.com>
2024-09-10 20:51:01 +0300

    ,

--- PROGRAMARE/WORKSPACE/C++/main1.cpp
+++ PROGRAMARE/WORKSPACE/C++/main1.cpp
@@ -1,19 +1,6 @@
-struct nod
-{
-    int info;
-    nod *ant, *urm;
-};
+#include <iostream>
+using namespace std;
 
-bool Palindrom(nod *prim, nod *ultim)
+int main()
 {
-    while (prim != ultim)
-    {
-        if (prim->info != ultim->info)
-        {
-            return false;
-        }
-        prim = prim->urm;
-        ultim = ultim->ant;
-    }
-    return true;
 }
