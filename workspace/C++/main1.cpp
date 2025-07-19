bcba99357573cf8c92ed0cef93f8c2d222459522
Limit1500 <146538463+Limit1500@users.noreply.github.com>
2025-07-19 15:20:27 +0300

    ,

--- workspace/C++/main1.cpp
+++ workspace/C++/main1.cpp
@@ -1,65 +1,24 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
-
-ifstream input("info.in");
-ofstream output("plaja.out");
-
-int nrLinii, nrColoane, arieMax = 0;
-int hist[100];
-
-void checkHist()
-{
-    hist[nrColoane] = 0;
-    int stiva[100], lungimeStiva = 0;
-    for (int i = 0; i <= nrColoane; i++)
-    {
-        if (lungimeStiva == 0 || hist[stiva[lungimeStiva - 1]] <= hist[i])
-        {
-            stiva[lungimeStiva++] = i;
-        }
-        else
-        {
-            while (lungimeStiva && hist[stiva[lungimeStiva - 1]] > hist[i])
-            {
-                int arie = hist[stiva[lungimeStiva - 1]] * ((lungimeStiva >= 2) ? (i - 1 - stiva[lungimeStiva - 2]) : i);
-                if (arie > arieMax)
-                {
-                    arieMax = arie;
-                    cout << arie << ' ';
-                }
-                lungimeStiva--;
-            }
-            stiva[lungimeStiva++] = i;
-        }
-    }
-}
 
 int main()
 {
-    input >> nrLinii >> nrColoane;
-    for (int i = 0; i < nrLinii; i++)
-    {
-        char numar;
-        input.get(numar);
-        for (int j = 0; j < nrColoane; j++)
-        {
-            input.get(numar);
-            if (numar == '1')
-            {
-                hist[j] = 0;
-            }
-            else
-            {
-                hist[j]++;
-            }
-        }
+    int n;
+    cin >> n;
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j == i || i + j == n - 1)
+                cout << 0 << " ";
+            else if (i + j < n - 1 && j > i)
+                cout << 1 << " ";
+            else if (i + j > n - 1 && i > j)
+                cout << 2 << " ";
+            else
+                cout << 3 << " ";
+        }
         cout << '\n';
-        cout << "Se calculeaza ariile pentru histul: ";
-        for (int j = 0; j < nrColoane; j++)
-            cout << hist[j] << ' ';
-        cout << '\n';
-        checkHist();
     }
 }
